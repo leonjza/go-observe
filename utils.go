23d22525604adb3d5504cb8894cabcd4add15ea2
Leon Jacobs <leonja511@gmail.com>
2017-02-07 13:06:38 +0200

    Allow hostnames to be submitted along with urls

--- utils.go
+++ utils.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 func validateAndGetURLHost(urlSample string) (string, error) {
 
+	// cheat a little by checking if we have :// in the string.
+	// if we dont, its probably a hostname already
+	if !strings.Contains("://", urlSample) {
+
+		s := strings.TrimSpace(urlSample)
+		return s, nil
+	}
+
+	// otherwise, if there is a ://, try and extract the hostname
 	parsed, err := url.ParseRequestURI(urlSample)
 
 	if err != nil {
